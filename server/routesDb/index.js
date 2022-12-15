7c0b4282710f7931af9f02160504d1f801079957
Ivan <dev@fedresurs.online>
2022-12-15 09:50:59 +0300

    last test export

--- server/routesDb/index.js
+++ server/routesDb/index.js
@@ -459,7 +459,7 @@ module.exports = function(app, upload, jwt) {
 
     app.post('/fieldsExport', async (req, res) => {
         const { page, idCore, max } = req.body
-        let limit = 10
+        let limit = 100
         let offset = limit * page - limit
         let isOfssetAboveMax = false
 
@@ -487,27 +487,8 @@ module.exports = function(app, upload, jwt) {
                 offset,
                 include: [{
                     model: Scheme.typeOfField,
-                    // include: [{
-                    //     model: Scheme.coreTypeOfField,
-                    //     where: {
-                    //         value: {
-                    //             [Op.ne]: ""
-                    //         }
-                    //     }
-                    // }]
                 }]
             })
-
-            // for(let i = 0; i < theCores.length; i++) {
-            //     let aTheCore = theCores[i]
-
-            //     theCores[i].typeOfFields = await aTheCore.getTypeOfFields()
-
-            //     for(let g = 0; g < theCores[i].typeOfFields.length; g++) {
-            //         let t = await theCores[i].typeOfFields[g].getCoreTypeOfFields()
-            //         theCores[i].typeOfFields[g].coreTypeOfField = t[0]
-            //     }
-            // }
 
             res.json({ 
                 name: aCore.name,
